71d434e705299343a8bf4abb67806701f242ad5f
Jason Dreisbach <jtdreisb@gmail.com>
2013-02-14 11:14:54 -0800

    adding new changes to the netfunnel and mp32wave execuatbles

--- Audio/pd/mp32wav.sh
+++ Audio/pd/mp32wav.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/ksh
 # usage: ./mp32wav.sh /path/to/song.mp3
 # result: ./song.mp3.wav
 
+if [ ! -d "./samples" ]; then
+	mkdir "samples"
+fi
 
-CMD="afconvert -d LEI16 -f 'WAVE' \"$1\" -o \"`basename $1`.wav\""
+CMD="afconvert -d LEI16 -f 'WAVE' \"$1\" -o \"samples/`basename $1`.wav\""
 echo $CMD
 eval $CMD
