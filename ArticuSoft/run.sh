883c3901108deae676894943102c7d3b17029ba7
unknown <anthony.lipscomb@SL-WL-053.SLO.MBO.AD>
2013-03-05 15:24:47 -0800

    Remove old front-end files

--- ArticuSoft/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -f "/Library/Java/Extensions/RXTXcomm.jar" ]; then
-
-	echo "sudo ditto references/RxTx/RXTXcomm.jar /Library/Java/Extensions/"
-	echo "sudo ditto references/RxTx/Mac_OS_X/librxtxSerial.jnilib /Library/Java/Extensions/"
-fi
-
-java -jar -d32 Articusoft.jar Main
